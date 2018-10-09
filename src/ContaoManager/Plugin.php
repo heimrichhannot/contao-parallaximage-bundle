7c215520819ce267f3f2834b6ac50fc15e1882b7
Thomas Körner <t.koerner@heimrich-hannot.de>
2018-10-09 10:18:04 +0200

    added encore config

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -18,10 +18,13 @@ use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Config\ConfigInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Config\ConfigPluginInterface;
+use Contao\ManagerPlugin\Config\ContainerBuilder;
+use Contao\ManagerPlugin\Config\ExtensionPluginInterface;
 use HeimrichHannot\ContaoParallaxImageBundle\HeimrichHannotContaoParallaxImageBundle;
+use HeimrichHannot\UtilsBundle\Container\ContainerUtil;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class Plugin implements BundlePluginInterface, ConfigPluginInterface
+class Plugin implements BundlePluginInterface, ConfigPluginInterface, ExtensionPluginInterface
 {
 
 	/**
@@ -42,5 +45,22 @@ class Plugin implements BundlePluginInterface, ConfigPluginInterface
 	public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig)
 	{
 		$loader->load('@HeimrichHannotContaoParallaxImageBundle/Resources/config/services.yml');
+	}
+
+	/**
+	 * Allows a plugin to override extension configuration.
+	 *
+	 * @param string $extensionName
+	 *
+	 * @return array<string,mixed>
+	 */
+	public function getExtensionConfig($extensionName, array $extensionConfigs, ContainerBuilder $container)
+	{
+		return ContainerUtil::mergeConfigFile(
+			'huh_encore',
+			$extensionName,
+			$extensionConfigs,
+			__DIR__.'/../Resources/config/config_encore.yml'
+		);
 	}
 }
