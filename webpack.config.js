1d13747e453216f02781ffbd3589218dcdd14615
Thomas Körner <t.koerner@heimrich-hannot.de>
2025-06-25 11:46:09 +0200

    update bundles assets and build tools

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-.setOutputPath('src/Resources/public/assets/')
+.setOutputPath('public/assets/')
 .setPublicPath('/bundles/heimrichhannotcontaoparallaximage')
 .setManifestKeyPrefix('bundles/heimrichhannotcontaoparallaximage')
-.addEntry('contao-parallaximage-bundle', './src/Resources/assets/js/contao-parallaximage-bundle.js')
+.addEntry('contao-parallaximage-bundle', './assets/js/contao-parallaximage-bundle.js')
 .disableSingleRuntimeChunk()
 .addExternals({
     'rellax': 'Rellax'
